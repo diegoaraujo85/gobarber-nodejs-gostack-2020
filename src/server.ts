6315889d8b489aaa3638c7e0daeb5c84c0907cc1
Diego Araujo <doasalah@gmail.com>
2020-04-14 17:29:21 -0300

    debug

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import routes from './routes';
 
-
 const app = express();
 const PORT = 3333;
-
+app.use(express.json());
 app.use(routes);
-
 
 app.listen(PORT, () => {
   console.log(`🚀️ Server started on port ${PORT}`);
